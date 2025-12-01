3c6c3fa88c59c958f7ecc39c6eed6c8648646f09
John Wood <john.wood@arondite.com>
2025-12-01 21:58:21 +0000

    init

---
+++ src/bin/day01.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = include_str!("../../input/day01.txt");
+    
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    todo!()
+}
+
+fn part2(input: &str) -> usize {
+    todo!()
+}
+
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Advent of Code 2025!");
+    println!();
+    println!("Run a specific day with:");
+    println!("  cargo run --bin day01");
+    println!("  cargo run --bin day02");
+    println!("  ...");
+    println!();
+    println!("Run tests for a day with:");
+    println!("  cargo test --bin day01");
+}
